840d1ebc7bd81cbd71c0242c4d7c5b84f045aad9
skadam2041 <ssk952041@gmail.com>
2023-12-25 18:21:22 +0530

    refactored as production level code

--- BuilderDesignPattern/Client.java
+++ BuilderDesignPattern/Client.java
@@ -2,7 +2,7 @@ public class Client {
     public static void main(String[] args) {
         Student student = Student.getBuilder()
             .setName("John")
-            .setAge(1000)
+            .setAge(90)
             .setAddress("1234 Main St")
             .build();
 
--- BuilderDesignPattern/Student.java
+++ BuilderDesignPattern/Student.java
@@ -2,7 +2,7 @@ public class Student {
     private String name;
     private int age;
     private String address;
-    public Student(Builder builder){
+    private Student(Builder builder){
         this.name = builder.getName();
         this.age = builder.getAge();
         this.address = builder.getAddress();
@@ -14,6 +14,57 @@ public class Student {
 
     public String toString(){
         return "Name: " + this.name + "\nAge: " + this.age + "\nAddress: " + this.address;
+    }
+
+
+    public static class Builder {
+        private String name;
+        private int age;
+        private String address;
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Student build() {
+            if (!validate()) {
+                throw new IllegalArgumentException("Invalid student");
+            }
+            return new Student(this);
+        }
+
+        public boolean validate() {
+            if (this.name == null || this.name.isEmpty()) {
+                return false;
+            }
+            if (this.age < 0 || this.age > 100) {
+                return false;
+            }
+            if (this.address == null || this.address.isEmpty()) {
+                return false;
+            }
+            return true;
+        }
     }
 
 
